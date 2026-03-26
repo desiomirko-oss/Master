1bc00795373ae9e1d69b1e715f91b0e777fa40ba
Mirko <desio.mirko@gmail.com>
2026-03-26 21:02:47 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,36 +1,36 @@
 const APP_CONFIG = {
-    testoLogo: "MAESTRO AR",
+    testoLogo: "EXPERIENCE AR",
     colori: { principale: "#c0392b" }
 };
 
 const MENU_DATA = {
     food: [
         { 
-            id: "piatti", 
+            id: "specialità", 
             piatti: [
                 { 
-                    n: "Salmone scottato", 
-                    p: "16€", 
-                    d: "Salmone fresco con crosta di sesamo e asparagi.", 
+                    n: "Salmone al Pepe Rosa", 
+                    p: "18€", 
+                    d: "Salmone fresco scottato con granella di pepe rosa e asparagi croccanti.", 
                     f: "https://images.unsplash.com/photo-1467003909585-2f8a72700288?w=600",
-                    m: "test.glb", 
-                    glutenFree: true // Abilita il filtro GF
+                    m: "test.glb", // Assicurati che il nome file sia identico su GitHub
+                    glutenFree: true 
                 }
             ]
         }
     ],
     drinks: [
         { 
-            id: "bar", 
+            id: "cocktails", 
             piatti: [
                 { 
-                    n: "Cocktail del Bosco", 
-                    p: "9€", 
-                    d: "Frutti rossi bio, soda e menta.", 
+                    n: "Virgin Mojito", 
+                    p: "7€", 
+                    d: "Menta fresca, lime, zucchero di canna e soda.", 
                     f: "", 
                     m: "", 
-                    bio: true,       // Abilita filtro Bio
-                    analcolico: true // Abilita filtro Analcolico
+                    bio: true,
+                    analcolico: true 
                 }
             ]
         }

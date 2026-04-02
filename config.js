66b03a54c538b1784bb6891529eb5fa99479a4e7
Mirko <desio.mirko@gmail.com>
2026-04-02 21:15:59 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,69 +1,81 @@
 const APP_CONFIG = {
-    testoLogo: "IL MIO RISTORANTE",
+    testoLogo: "RESTAURANTE DOÑA LOLA", // [cite: 1]
+    sottotitolo: "Recetas alegres de la abuela - dal 2011", // [cite: 1]
     colori: { principale: "#000000" }
 };
 
 const MENU_DATA = {
     cibo: [
         {
-            id: "Antipasti",
-            italiano: [
-                {
-                    N: "Tagliere Misto",
-                    P: "15€",
-                    D: "Selezione di salumi e formaggi locali.",
-                    F: "https://images.selex...", 
-                    sg: true, // Use true instead of VERO
-                    v: false  // Use false instead of falso
-                }
+            id: "Pizzas", // [cite: 2, 5]
+            Italiano: [
+                { N: "Margarita", P: "9.90€", D: "Pomodoro, mozzarella, origano", sg: false, v: true }, // 
+                { N: "Bacon", P: "11.90€", D: "Pomodoro, mozzarella, cipolla, bacon, origano", sg: false, v: false }, // 
+                { N: "Funghi", P: "12.90€", D: "Pomodoro, mozzarella, cipolla, funghi, origano", sg: false, v: true }, // 
+                { N: "Vegetariana", P: "11.90€", D: "Pomodoro, mozzarella, cipolla, peperoni, funghi, origano", sg: false, v: true }, // 
+                { N: "Prosciutto", P: "12.90€", D: "Pomodoro, mozzarella, prosciutto cotto, origano", sg: false, v: false }, // 
+                { N: "Chorizo", P: "12.90€", D: "Pomodoro, mozzarella, chorizo, origano", sg: false, v: false }, // 
+                { N: "Diavola", P: "13.90€", D: "Pomodoro, mozzarella, chorizo piccante, peperone rosso, peperoncino", sg: false, v: false }, // 
+                { N: "Carbonara", P: "13.90€", D: "Pomodoro, mozzarella, bacon, uovo, cipolla, origano", sg: false, v: false }, // 
+                { N: "Gamberi", P: "13.90€", D: "Pomodoro, mozzarella, gamberi, aglio, prezzemolo", sg: false, v: false }, // 
+                { N: "4 Formaggi", P: "13.90€", D: "Pomodoro, mozzarella, roquefort, brie, manchego, origano", sg: false, v: true }, // 
+                { N: "Tonno e Gamberi", P: "14.90€", D: "Pomodoro, mozzarella, cipolla, tonno, gamberi, origano", sg: false, v: false }, // 
+                { N: "Doña Lola", P: "15.90€", D: "Pomodoro, mozzarella, pollo al curry, bacon, cipolla, origano", sg: false, v: false }, // 
+                { N: "Peperoni", P: "13.90€", D: "Pomodoro, mozzarella, peperoni, origano", sg: false, v: true }, // 
+                { N: "Caprese", P: "15.90€", D: "Pomodoro, mozzarella, pomodorini, rucola e mozzarella fresca", sg: false, v: true } // 
             ]
-        { 
-                    n: "Caprese Bufala", 
-                    p: "12€", 
-                    d: "Mozzarella di bufala campana DOP e pomodorini.", 
-                    f: "https://www.lilvienna.com/wp-content/uploads/Recipe-Classic-Italian-Caprese-Salad.jpg", 
-                    a: "Lattosio",
-                    sg: true,
-                    v: true // È vegetariano
+        },
+        {
+            id: "Pasta & Lasagna", // [cite: 3, 7]
+            Italiano: [
+                { N: "Bolognese", P: "12.90€", D: "Spaghetti o maccheroni alla bolognese", sg: false, v: false }, // [cite: 10]
+                { N: "Carbonara", P: "14.90€", D: "Spaghetti o maccheroni alla carbonara", sg: false, v: false }, // [cite: 10]
+                { N: "Marinara", P: "15.90€", D: "Spaghetti o maccheroni ai frutti di mare", sg: false, v: false }, // [cite: 10]
+                { N: "Quattro Formaggi", P: "13.90€", D: "Spaghetti o maccheroni ai quattro formaggi", sg: false, v: true }, // [cite: 10]
+                { N: "Verdura Fresca", P: "12.00€", D: "Spaghetti o maccheroni con verdure fresche", sg: false, v: true }, // [cite: 10]
+                { N: "Tartufo e Funghi", P: "15.90€", D: "Spaghetti o maccheroni con tartufo e funghi", sg: false, v: true }, // [cite: 10]
+                { N: "Gamberi e Aglio", P: "12.90€", D: "Spaghetti o maccheroni con gamberi e aglio", sg: false, v: false }, // [cite: 10]
+                { N: "Lasagna Bolognese", P: "15.90€", D: "Lasagna classica al ragù", sg: false, v: false }, // [cite: 10]
+                { N: "Lasagna Verdure", P: "14.90€", D: "Lasagna alle verdure", sg: false, v: true } // [cite: 10]
+            ]
+        },
+        {
+            id: "Noodles", // [cite: 11]
+            Italiano: [
+                { N: "Noodles Verdura e Gamberi", P: "16.00€", D: "Noodles con verdure e gamberi", sg: false, v: false }, // [cite: 11]
+                { N: "Noodles Verdura e Manzo", P: "17.00€", D: "Noodles con verdure e carne di manzo", sg: false, v: false }, // [cite: 11]
+                { N: "Noodles Misti", P: "18.00€", D: "Noodles con verdure, gamberi e manzo", sg: false, v: false } // [cite: 11]
+            ]
+        },
+        {
+            id: "Insalate", // [cite: 12]
+            Italiano: [
+                { N: "Insalata Mista con Tonno", P: "12.90€", D: "Insalata mista classica con tonno", sg: true, v: false }, // [cite: 13]
+                { N: "Insalata Peperoni e Tonno", P: "10.90€", D: "Peperoni arrostiti marinati con tonno", sg: true, v: false }, // [cite: 13]
+                { N: "Insalata César", P: "13.90€", D: "Lattuga, pollo, crostini e salsa Caesar", sg: false, v: false }, // [cite: 13]
+                { N: "Insalata Formaggio di Capra", P: "14.00€", D: "Insalata con formaggio di capra caldo", sg: true, v: true }, // [cite: 13]
+                { N: "Insalata Doña Lola", P: "14.90€", D: "Specialità della casa", sg: true, v: false } // [cite: 13]
+            ]
+        },
+        {
+            id: "Hamburger", // [cite: 14]
+            Italiano: [
+                { 
+                    N: "Pollo Croccante", 
+                    P: "15.90€", 
+                    D: "Pomodoro, lattuga, formaggio di capra e cipolla caramellata", // [cite: 17, 18]
+                    sg: false, v: false 
                 },
                 { 
-                    n: "Lasagna Bolognese", 
-                    p: "19€", 
-                    d: "Piatto tipico italiano con pasta fresca.", 
-                    f: "https://www.giallozafferano.it/images/229-22941/Lasagne-alla-Bolognese_650x433_wm.jpg",  
-                    a: "Lattosio",
-                    sg: true, // true se è Senza Glutine
-                    v: true  // true se è Vegetariano
-            ]
-        },
-        { 
-            id: "Primi", 
-            piatti: [ 
-                { 
-                    n: "Spaghetti al Pomodoro", 
-                    p: "10€", 
-                    d: "Pasta trafilata al bronzo, pomodoro San Marzano.", 
-                    f: "https://media-assets.lacucinaitaliana.it/photos/6411f3da3d6fc4cae3c3892b/16:9/w_3300,h_1856,c_limit/photo-07201503701.jpg", 
-                    a: "Glutine",
-                    sg: false,
-                    v: true
-                },
-                { 
-                    n: "Tortellini Emiliani", 
-                    p: "11€", 
-                    d: "Pasta fatta in casa.", 
-                    f: "https://d2sj0xby2hzqoy.cloudfront.net/kenwood_italy/attachments/data/000/010/046/medium/tortellini-brodo.jpg", 
-                    a: "Glutine",
-                    sg: false,
-                    v: false
+                    N: "Manzo", 
+                    P: "15.90€", 
+                    D: "Pomodoro, lattuga, cipolla rossa, bacon e formaggio americano", // [cite: 23, 24]
+                    sg: false, v: false 
                 }
             ]
-        },
-         {
-             id: "Secondi", 
-            piatti: [ ]
-        },
-    ],
+        }
+    ]
+};
     drinks: [
         { 
             id: "Cocktails", 
